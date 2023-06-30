b2034d4874c2cd731df902f9c8f5bd85c8eac1e8
Ross Barnowski <rossbar@caltech.edu>
2023-06-30 01:24:28 -0700

    Modify script to generate marker-positivity jobs.

--- hubmap_raw_to_dcl.py
+++ hubmap_raw_to_dcl.py
@@ -61,7 +61,7 @@ def hubmap_hickey_to_dcl(data_dir, image_path, output_path=None):
     # Rel path from data_dir to img, e.g. /processed/bestFocus/reg001...
     image_path = Path(image_path)
     image_fname = image_path.name
-    out_fname = os.path.splitext(image_fname)[0] + "_project.zip"
+    out_fname = os.path.splitext(image_fname)[0] + "_mpm_project.zip"
     # Determine output path given data-dir and img-path
     if output_path is None:
         output_path = data_dir / "/".join(image_path.parts[:-1])
@@ -80,45 +80,21 @@ def hubmap_hickey_to_dcl(data_dir, image_path, output_path=None):
 
     # Get the channel names for the dataset
     with open(data_dir / "channelnames.txt", "r") as fh:
-        data_chnames = [l.rstrip() for l in fh.readlines()]
+        data_chnames = [l.rstrip().upper() for l in fh.readlines()]
 
     # Create some mappings from channel names to img index
-    data_ch_to_idx = {name: idx for idx, name in enumerate(data_chnames)}
-    data_ch_to_idx_upper = {k.upper(): v for k, v in data_ch_to_idx.items()}
-    data_chset = set(data_ch_to_idx_upper)
+    data_ch_to_idx = {
+        name: idx for idx, name in enumerate(data_chnames)
+    }
 
     # Channels to drop from consideration from the raw data
-    empties = [ch for ch in data_chnames if ch.upper().startswith("EMPTY")]
-    blanks = [ch for ch in data_chnames if ch.upper().startswith("BLANK")]
-    hoechsts = [ch for ch in data_chnames if ch.upper().startswith("HOECHST")]
+    empties = [ch for ch in data_chnames if ch.startswith("EMPTY")]
+    blanks = [ch for ch in data_chnames if ch.startswith("BLANK")]
+    hoechsts = [ch for ch in data_chnames if ch.startswith("HOECHST")]
     hoechsts_to_drop = hoechsts[1:]
     # Drop em
     for k in empties + blanks + hoechsts_to_drop:
         data_ch_to_idx.pop(k)
-
-    with open("/home/administrator/repos/deepcelltypes-hubmap/model/config.yaml") as fh:
-        model_config = yaml.load(fh, yaml.Loader)
-
-    # Get model channel names
-    model_chnames = model_config["channels"]
-    model_chset = set(model_chnames)
-
-    # Manually inspect the data and model channel sets
-    model_chset & data_chset
-    model_chset - data_chset
-    data_chset - model_chset
-
-    # Create a mapping from model_channel to raw data image index
-    # NOTE: the m_ch.upper() is necessary for CD49a -> CD49A
-    model_ch_to_idx = {
-        m_ch: data_ch_to_idx_upper[m_ch.upper()]
-        for m_ch in model_chnames
-        if m_ch.upper() in data_ch_to_idx_upper
-    }
-    # A few manual additions: Hoechst1 and cytokeratin for segmentation
-    # sanity check
-    model_ch_to_idx["PANCK"] = data_ch_to_idx_upper["CYTOKERATIN"]
-    model_ch_to_idx["Hoechst1"] = data_ch_to_idx["Hoechst1"]
 
     # Load multiplexed image
     img = tff.imread(data_dir / image_path)
@@ -139,8 +115,8 @@ def hubmap_hickey_to_dcl(data_dir, image_path, output_path=None):
     # as nuclear and whole cell channels, respectively
     segmentation_input = np.stack(
         [
-            img[model_ch_to_idx["Hoechst1"]],
-            img[model_ch_to_idx["PANCK"]] + img[model_ch_to_idx["VIMENTIN"]],
+            img[data_ch_to_idx["HOECHST1"]],
+            img[data_ch_to_idx["CYTOKERATIN"]] + img[data_ch_to_idx["VIMENTIN"]],
         ],
         axis=-1
     )[np.newaxis, ...]
@@ -155,8 +131,8 @@ def hubmap_hickey_to_dcl(data_dir, image_path, output_path=None):
     print(f"Number of cells in {image_fname}: {number_of_cells}")
 
     # Create the multplexed image from the model/channel mapping
-    multiplexed_img = img[list(model_ch_to_idx.values())]
-    channels = list(model_ch_to_idx)
+    multiplexed_img = img[list(data_ch_to_idx.values())]
+    channels = list(data_ch_to_idx)
 
     # Normalize to range 0-255 and convert to uint8
     chmax = multiplexed_img.max(axis=(1, 2), keepdims=True)
@@ -167,12 +143,7 @@ def hubmap_hickey_to_dcl(data_dir, image_path, output_path=None):
 
     # Get cell types from model config
     import utils
-    model_config["cell_types"]
-    # Ignore BACKGROUND category
-    # cell_types = model_config["cell_types"][1:]
-    # TODO: Modify make_empty_cell_types to read from model configuration
-    # instead of `constants` module
-    cell_types = utils.make_empty_cell_types()
+    cell_types = utils.make_empty_marker_positivity(list(data_ch_to_idx))
     # TODO: Add other categories for annotation (e.g. UNSURE)
 
 
