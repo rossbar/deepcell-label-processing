1af263ce5c7f26f447a6bf7643a61d8c79c312b6
Ross Barnowski <rossbar@caltech.edu>
2023-07-06 02:34:39 -0700

    Add CLI and input validation on mask shape.

--- Keren_New_MIBI.py
+++ Keren_New_MIBI.py
@@ -2,58 +2,77 @@ import os
 from pathlib import Path
 import numpy as np
 import yaml
+import click
 
 import utils
 from raw_to_dcl import dcl_zip
 
-data_path = Path("/data/data-registry/data/labels/static/2d/Tissue-Breast/Keren_New_MIBI")
-fname = "FOV410.npz"
-outpath = Path("/home/administrator/data-registry_dcl_outputs/") / "/".join(data_path.parts[-2:])
-out_fname = os.path.splitext(fname)[0] + "_mpm_project.zip"
 
-# Load data
-data = np.load(data_path / fname)
-# Corresponding metadata
-with open(data_path / (fname + ".dvc")) as fh:
-    metadata = yaml.load(fh, yaml.Loader)
-
-# Load index-to-marker mapping from metadata
-idx_to_ch = {
-    ch["index"]: ch["target"] for ch in
-    metadata["meta"]["sample"]["channels"]
-}
-channels = list(idx_to_ch.values())
+@click.command()
+@click.argument("data_path")
+@click.argument("image_fname")
+def cli(data_path, image_fname):
+    single_registry_image_to_dcl_project(data_path, image_fname)
 
 
-# Load image and cast to float
-img = data["X"].astype(float)
-mask = data["y"]
-print(f"Image shape: {img.shape}")
-print(f"Number of cells: {mask.max()}")
+def single_registry_image_to_dcl_project(data_path, fname):
+    # Munge outputs - TODO: un-hardcode
+    data_path = Path(data_path)
+    outpath = Path("/home/administrator/data-registry_dcl_outputs/")
+    outpath /= "/".join(data_path.parts[-2:])
+    out_fname = os.path.splitext(fname)[0] + "_mpm_project.zip"
 
-# Filter out empty channels
-empty_ch_mask = img.sum(axis=(0, 1, 2)) == 0
-if np.any(empty_ch_mask):
-    channels = np.asarray(channels)
-    print(
-        (
-            f"The following channels are empty and will be dropped:\n"
-            f"\t{channels[empty_ch_mask]}"
+    # Load data
+    data = np.load(data_path / fname)
+    # Corresponding metadata
+    with open(data_path / (fname + ".dvc")) as fh:
+        metadata = yaml.load(fh, yaml.Loader)
+
+    # Load index-to-marker mapping from metadata
+    idx_to_ch = {
+        ch["index"]: ch["target"] for ch in
+        metadata["meta"]["sample"]["channels"]
+    }
+    channels = list(idx_to_ch.values())
+
+
+    # Load image and cast to float
+    img = data["X"].astype(float)
+    mask = data["y"]
+    assert len(mask.shape) == 4
+    if mask.shape[0] != 1 or mask.shape[-1] != 1:
+        raise ValueError(
+            f"Multiple masks found, mask shape: {mask.shape}"
         )
-    )
-    channels = channels[~empty_ch_mask].tolist()
-    img = img[..., ~empty_ch_mask]
+    print(f"Image shape: {img.shape}")
+    print(f"Number of cells: {mask.max()}")
 
-# Normalize and convert to uint8
-chmax = img.max(axis=(1, 2), keepdims=True)
-chmin = img.min(axis=(1, 2), keepdims=True)
-X = ((img - chmin) / (chmax - chmin) * 255).astype(np.uint8)
-# ometiff is very picky about dimensions
-# TODO: figure out why
-X = X.transpose((3, 0, 1, 2))
-y = mask.transpose((0, 3, 1, 2))
+    # Filter out empty channels
+    empty_ch_mask = img.sum(axis=(0, 1, 2)) == 0
+    if np.any(empty_ch_mask):
+        channels = np.asarray(channels)
+        print(
+            (
+                f"The following channels are empty and will be dropped:\n"
+                f"\t{channels[empty_ch_mask]}"
+            )
+        )
+        channels = channels[~empty_ch_mask].tolist()
+        img = img[..., ~empty_ch_mask]
 
-# Marker-positivity "cell types"
-cell_types = utils.make_empty_marker_positivity(channels)
+    # Normalize and convert to uint8
+    chmax = img.max(axis=(1, 2), keepdims=True)
+    chmin = img.min(axis=(1, 2), keepdims=True)
+    X = ((img - chmin) / (chmax - chmin) * 255).astype(np.uint8)
+    # ometiff is very picky about dimensions
+    # TODO: figure out why
+    X = X.transpose((3, 0, 1, 2))
+    y = mask.transpose((0, 3, 1, 2))
 
-dcl_zip(X, y, cell_types, channels, fname=outpath / out_fname)
+    # Marker-positivity "cell types"
+    cell_types = utils.make_empty_marker_positivity(channels)
+
+    dcl_zip(X, y, cell_types, channels, fname=outpath / out_fname)
+
+if __name__ == "__main__":
+    cli()
