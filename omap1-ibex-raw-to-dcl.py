c7f6150b3b26403b8c50fbe784f8205ed826dbe5
Ross Barnowski <rossbar@caltech.edu>
2024-01-30 14:15:46 -0800

    Add conversion for omap1 dataset.

---
+++ omap1-ibex-raw-to-dcl.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+plt.ion()
+import numpy as np
+import pandas as pd
+from deepcell.applications import Mesmer
+from pathlib import Path
+import yaml
+import itertools
+import os
+from imaris_ims_file_reader.ims import ims
+
+from raw_to_dcl import dcl_zip
+import constants
+
+def ravel_dict(d):
+    leafs = []
+    for k, v in d.items():
+        if v == {}:
+            leafs.append(k)
+        else:
+            leafs.extend(ravel_dict(v))
+    return leafs
+
+
+def make_empty_cell_types(ctlist):
+    """ Return cellTypes.json with master cell types list but no labels """
+    cell_types_json = []
+    colors = itertools.cycle(constants.COLOR_MAP)
+    for i, ct in enumerate(ctlist):
+        cell_types_json.append({'id': i+1, 'cells': [], 'color': next(colors),
+                               'name': ct, 'feature': 0})
+    return cell_types_json
+
+data_dir = Path("/data2/omap-reference-datasets/IBEX")
+dataset = "Human_Lymph_Node_Automated_IBEX.ims"
+output_dir = Path.home() / f"dryad_dcl_outputs/omap1/lymphnode_ibex_automated"
+output_dir.mkdir(exist_ok=True, parents=True)
+
+# Multiplex img
+ims_data = ims(data_dir / dataset)
+# Check square pixels
+assert ims_data.resolution[-2] == ims_data.resolution[-1]
+mpp = ims_data.resolution[-1]  # Pixel size, in microns (hopefully)
+img = ims_data[0, :, 0, :, :]  # res, ch, time, x, y
+
+# Channel names capitalized
+df = pd.read_excel(data_dir / dataset.replace(".ims", ".xlsx"))
+# NOTE: the startswith filter handles duplicate channels
+data_chnames = np.array(
+    [name.upper() for name in df["Target Name"] if not name.startswith("(")]
+)
+assert len(data_chnames) == img.shape[0]
+
+# Sort marker channels alphabetically
+data_ch_to_idx = {name: idx for idx, name in enumerate(data_chnames)}
+data_ch_to_idx = dict(sorted(data_ch_to_idx.items()))
+ch_to_idx = {k: v for v, k in enumerate(data_ch_to_idx)}
+
+# Prepare segmentation input
+segmentation_input = np.stack(
+    [
+        img[ch_to_idx["HOECHST 33342"]],
+        img[ch_to_idx["CD45"]],
+    ],
+    axis=-1,
+)[np.newaxis, ...]
+
+# Segment
+app = Mesmer()
+mask = app.predict(segmentation_input, image_mpp=mpp)
+y = mask.transpose(3, 0, 1, 2).astype(np.int32)
+
+# Reformat image for dcl
+chmax = img.max(axis=(1, 2), keepdims=True)
+chmin = img.min(axis=(1, 2), keepdims=True)
+X = ((img - chmin) / (chmax - chmin) * 255).astype(np.uint8)
+X = X[:, np.newaxis, :, :]
+
+# Get cell types
+ct_config_file = Path.home() / "Downloads/core_tree.yaml"
+with open(ct_config_file) as fh:
+    ctdata = yaml.load(fh, yaml.Loader)
+ctlist = ravel_dict(ctdata)
+cell_types = make_empty_cell_types(ctlist)
+
+# Channel names
+channels = list(data_ch_to_idx)
+
+# Original tiling scheme: 7x9 grid - 63 tiles total
+xstep = X.shape[2] // 14
+ystep = X.shape[3] + 1 // 14
+for i in range(0, X.shape[2], xstep):
+    for j in range(0, X.shape[3], ystep):
+        print(f"{i}:{i+xstep}, {j}:{j+ystep}")
+        dcl_zip(
+            X[..., i:i+xstep, j:j+ystep],
+            y[..., i:i+xstep, j:j+ystep],
+            cell_types,
+            channels,
+            fname=output_dir / f"tile_{i}-{i+xstep}_{j}-{j+ystep}.zip"
+        )
